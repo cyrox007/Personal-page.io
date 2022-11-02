774572d833859c788eb9fcc1f4574c3f0e44c6fb
cyrox007 <cyrox007@gmail.com>
2022-11-02 10:14:20 +0300

    update

--- sw.js
+++ sw.js
@@ -1,15 +1,3 @@
-const CACHE_NAME = "my-web-app";
-let urlCache = [
-  "/",
-  "/style/style.css",
-  "/scripts/main.js",
-  "/images/main-bg.webp",
-  "/images/black.webp",
-  "/images/blog.webp",
-  "/images/page.webp",
-  "/images/store.webp",
-  "/video/2.webm",
-];
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
