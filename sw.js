dc0f797b2109b2341848e725ae8f5d728fb8145e
cyrox007 <cyrox007@gmail.com>
2022-11-02 10:22:52 +0300

    еые

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('v1').then((cache) => {
       return cache.addAll([
-        "/style/style.css",
-        "/scripts/main.js",
-        "/images/main-bg.webp",
-        "/images/black.webp",
-        "/images/blog.webp",
-        "/images/page.webp",
-        "/images/store.webp",
-        "/video/2.webm",
+        "style/style.css",
+        "scripts/main.js",
+        "images/main-bg.webp",
+        "images/black.webp",
+        "images/blog.webp",
+        "images/page.webp",
+        "images/store.webp",
+        "video/2.webm",
       ]);
     })
   );
